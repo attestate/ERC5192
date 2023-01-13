f05740c3ff002b553847a577276d9e20b5ddbe13
Tim Daubenschütz <tim@daubenschuetz.de>
2023-01-13 11:20:24 +0100

    init

---
+++ test/ERC5192.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import {ERC5192} from "../src/ERC5192.sol";
+
+contract ERC5192Test is Test {
+    function setUp() public {
+    }
+}
