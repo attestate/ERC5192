e3c370520c71f493282ff90942ad607d70775930
Tim Daubenschütz <tim@daubenschuetz.de>
2023-01-13 11:53:12 +0100

    Implement ERC5192 and test as locked token

--- src/ERC5192.sol
+++ src/ERC5192.sol
@@ -1,5 +1,77 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.13;
 
-abstract contract ERC5192 {
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import {IERC5192} from "./IERC5192.sol";
+
+abstract contract ERC5192 is ERC721 {
+  bool private isLocked;
+
+  error ErrLocked();
+  error ErrNotFound();
+
+  constructor(string memory _name, string memory _symbol, bool _isLocked)
+    ERC721(_name, _symbol)
+  {
+    isLocked = _isLocked;
+  }
+
+  modifier checkLock() {
+    revert ErrLocked();
+    _;
+  }
+
+  function locked(uint256 tokenId) external view returns (bool) {
+    if (!_exists(tokenId)) revert ErrNotFound();
+    return isLocked;
+  }
+
+  function safeTransferFrom(
+    address from,
+    address to,
+    uint256 tokenId,
+    bytes memory data
+  ) public override checkLock {
+    safeTransferFrom(from, to, tokenId, data);
+  }
+
+  function safeTransferFrom(address from, address to, uint256 tokenId)
+    public
+    override
+    checkLock
+  {
+    safeTransferFrom(from, to, tokenId);
+  }
+
+  function transferFrom(address from, address to, uint256 tokenId)
+    public
+    override
+    checkLock
+  {
+    transferFrom(from, to, tokenId);
+  }
+
+  function approve(address approved, uint256 tokenId) public override checkLock {
+    approve(approved, tokenId);
+  }
+
+  function setApprovalForAll(address operator, bool approved)
+    public
+    override
+    checkLock
+  {
+    setApprovalForAll(operator, approved);
+  }
+
+  function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    virtual
+    override
+    returns (bool)
+  {
+    return interfaceId == type(IERC5192).interfaceId
+      || super.supportsInterface(interfaceId);
+  }
 }
--- test/ERC5192.t.sol
+++ test/ERC5192.t.sol
@@ -2,9 +2,84 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import {ERC5192} from "../src/ERC5192.sol";
+import {ERC721Holder} from
+  "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 
-contract ERC5192Test is Test {
-    function setUp() public {
-    }
+import {ERC5192} from "../src/ERC5192.sol";
+import {IERC5192} from "../src/IERC5192.sol";
+
+contract NTT is ERC5192 {
+  constructor(string memory _name, string memory _symbol, bool _isLocked)
+    ERC5192(_name, _symbol, _isLocked)
+  {}
+
+  function safeMint(address to, uint256 tokenId) external {
+    _safeMint(to, tokenId);
+  }
+}
+
+contract ERC5192Test is Test, ERC721Holder {
+  NTT lockedToken;
+
+  function setUp() public {
+    string memory name = "Name";
+    string memory symbol = "Symbol";
+    bool locked = true;
+    lockedToken = new NTT(name, symbol, locked);
+  }
+
+  function testCallingLocked() public {
+    address to = address(this);
+    uint256 tokenId = 0;
+    lockedToken.safeMint(to, tokenId);
+    assertTrue(lockedToken.locked(tokenId));
+  }
+
+  function testIERC5192() public {
+    assertTrue(lockedToken.supportsInterface(type(IERC5192).interfaceId));
+  }
+
+  function testLockedThrowingOnNonExistentTokenId() public {
+    vm.expectRevert(ERC5192.ErrNotFound.selector);
+    lockedToken.locked(1337);
+  }
+
+  function testBlockedSafeTransferFrom() public {
+    address from = address(0);
+    address to = address(this);
+    uint256 tokenId = 0;
+    lockedToken.safeMint(to, tokenId);
+
+    bytes memory data;
+    vm.expectRevert(ERC5192.ErrLocked.selector);
+    lockedToken.safeTransferFrom(address(this), address(1), tokenId, data);
+
+    vm.expectRevert(ERC5192.ErrLocked.selector);
+    lockedToken.safeTransferFrom(address(this), address(1), tokenId);
+  }
+
+  function testBlockedTransferFrom() public {
+    address from = address(0);
+    address to = address(this);
+    uint256 tokenId = 0;
+    lockedToken.safeMint(to, tokenId);
+
+    vm.expectRevert(ERC5192.ErrLocked.selector);
+    lockedToken.transferFrom(address(this), address(1), tokenId);
+  }
+
+  function testBlockedApprove() public {
+    address from = address(0);
+    address to = address(this);
+    uint256 tokenId = 0;
+    lockedToken.safeMint(to, tokenId);
+
+    vm.expectRevert(ERC5192.ErrLocked.selector);
+    lockedToken.approve(address(1), tokenId);
+  }
+
+  function testBlockedSetApprovalForAll() public {
+    vm.expectRevert(ERC5192.ErrLocked.selector);
+    lockedToken.setApprovalForAll(address(1), true);
+  }
 }
